03f7c1ea718fe32410491dd51f2b8deac24abdaa
MaWa313 <96030250+MaWa313@users.noreply.github.com>
2023-05-25 19:29:39 +0700

    Add files via upload

---
+++ Latihan_SD5/src/latihan_sd5/Node.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package latihan_sd5;
+
+/**
+ *
+ * @author asus tuf
+ */
+public class Node {
+
+    
+
+    int data;
+    Node left;
+    Node right;
+
+    public Node(int data){
+    this.data = data;
+    }
+    }
